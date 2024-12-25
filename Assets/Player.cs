5c9bb6a63d47f544cfb9a8b52ab5b6e71c30ac7f
hongngoc2003 <nhngoc1506@gmail.com>
2024-12-25 17:48:03 +0700

    finish player animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,27 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    public Rigidbody2D rb;
+public class Player : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Animator anim;
+
 
     [Header("Move info")]
-    public float moveSpeed;
-    public float jumpForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
 
-    private bool runBegin;
+    private bool playerStartToRun;
 
     [Header("Collision info")]
-    public LayerMask whatIsGround;
-    public float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckDistance;
     private bool isGrounded;
 
     private void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update() {
-        if (runBegin == true)
+        AnimatorController();
+
+        if (playerStartToRun == true)
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
 
         CheckCollision();
@@ -31,13 +35,19 @@ public class Player : MonoBehaviour
 
     }
 
+    private void AnimatorController() {
+        anim.SetBool("isGrounded",isGrounded);
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
     private void CheckCollision() {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void CheckInput() {
         if (Input.GetButtonDown("Fire2"))
-            runBegin = true;
+            playerStartToRun = true;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
