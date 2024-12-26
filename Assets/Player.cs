bb3c70d986499d805c1c24a951ff1df4fec29812
hongngoc2003 <nhngoc1506@gmail.com>
2024-12-26 19:50:28 +0700

    add slide

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
@@ -16,10 +15,23 @@ public class Player : MonoBehaviour {
 
     private bool playerStartToRun;
 
+    [Header("Slide info")]
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideTime;
+    [SerializeField] private float slideCooldown;
+    private float slideTimeCounter;
+    private float slideCooldownCounter;
+    private bool isSliding;
+
     [Header("Collision info")]
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float ceilingCheckDistance;
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Vector2 wallCheckSize;
     private bool isGrounded;
+    private bool wallDetected;
+    private bool ceilingDetected;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -28,47 +40,88 @@ public class Player : MonoBehaviour {
     }
 
     private void Update() {
+        CheckCollision();
         AnimatorController();
 
+        slideTimeCounter -= Time.deltaTime;
+        slideCooldownCounter -= Time.deltaTime;
+
         if (playerStartToRun == true)
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            Move();
 
-        CheckCollision();
+        if (isGrounded)
+            canDoubleJump = true;
 
+        CheckForSlide();
         CheckInput();
 
     }
 
+    private void CheckForSlide() {
+        if(slideTimeCounter < 0 && !ceilingDetected)
+            isSliding = false;
+    }
+
+    private void Move() {
+        if (wallDetected)
+            return;
+
+        if(isSliding)
+            rb.velocity = new Vector2(slideSpeed, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+
     private void AnimatorController() {
         anim.SetBool("canDoubleJump", canDoubleJump);
-        anim.SetBool("isGrounded",isGrounded);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isSliding", isSliding);
+        
         anim.SetFloat("xVelocity", rb.velocity.x);
         anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void CheckCollision() {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, whatIsGround);
+        ceilingDetected = Physics2D.Raycast(transform.position, Vector2.up, ceilingCheckDistance, whatIsGround);
     }
 
     private void CheckInput() {
         if (Input.GetButtonDown("Fire2"))
             playerStartToRun = true;
 
-        if (Input.GetButtonDown("Jump") )
+        if (Input.GetButtonDown("Jump"))
             JumpButton();
+
+        if (Input.GetKeyDown(KeyCode.F))
+            SlideButtonCheck();
+
+    }
+
+    private void SlideButtonCheck() {
+        if(rb.velocity.x != 0 || slideCooldownCounter < 0) {
+            isSliding = true;
+            slideTimeCounter = slideTime;
+            slideCooldownCounter = slideCooldown;
+        }
     }
 
     private void JumpButton() {
+        if (isSliding)
+            return;
+
         if (isGrounded) {
-            canDoubleJump = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        } else if(canDoubleJump) {
+        } else if (canDoubleJump) {
             canDoubleJump = false;
             rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
         }
     }
 
     private void OnDrawGizmos() {
-        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y -groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + ceilingCheckDistance));
     }
 }
